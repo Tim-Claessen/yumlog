da66f5a3a19fceee89f216b5785635e62ce26721
Tim Claessen <timclaessen96@gmail.com>
2025-06-25 22:01:58 +0800

    Obsidian auto sync: 2025-06-25 22:01:58

--- _tools/instagram_scrape.py
+++ _tools/instagram_scrape.py
@@ -3,7 +3,7 @@ import instaloader
 # Create an instance
 L = instaloader.Instaloader(download_comments=False, save_metadata=False, post_metadata_txt_pattern='')
 
-L.login('tim_claessen_', 'Jenae41103!')  # Save your credentials or use .load_session_from_file
+L.login('tim_claessen_', 'password')  # Save your credentials or use .load_session_from_file
 
 # Target profile to scrape
 target_profile = "__tablemanners__"
